92427daa325ffccab70a56faa85de92b0c23ce37
Alexander Ambrose <alexander.r.ambrose@gmail.com>
2015-06-27 00:03:39 -0400

    Ember-Intl inclusion

---
+++ app/routes/application.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  intl: Ember.inject.service(),
+
+  beforeModel() {
+    // define the app's runtime locale
+    // For example, here you would maybe do an API lookup to resolver
+    // which locale the user should be targeted and perhaps lazily
+    // load translations using XHR and calling intl's `addMessage`/`addMessages`
+    // method with the results of the XHR request
+    Ember.set(this, 'intl.locale', 'en-us');
+  }
+});
